9c40928dd97669384eeb6cfee40645fee50d8f38
U-pcline\Tilman <Tilman@pcline.(none)>
2013-12-08 01:56:07 +0100

    Added tools and some client code

---
+++ client/Mesh.cpp
@@ -0,0 +1,18 @@
+#ifndef __MESH_CPP__
+#define __MESH_CPP__
+
+Mesh::Mesh(const char *mesh_file, const char *tex_file)
+{
+		int[] textures = new int[1];
+        glGenTextures(1, textures, 0);
+        mTextureID = textures[0];
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureID);
+        
+        // When MAGnifying the image (no bigger mipmap available), use LINEAR filtering
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,GLES20.GL_LINEAR_MIPMAP_LINEAR);
+        // When MINifying the image, use a LINEAR blend of two mipmaps, each filtered LINEARLY too
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER,GLES20.GL_LINEAR);
+}
+
+
+#endif
---
+++ client/Mesh.h
@@ -0,0 +1,44 @@
+#ifndef __MESH_H__
+#define __MESH_H__
+
+#include <list>
+
+class Mesh
+{
+public:
+	int numverts;
+	int numtris;
+	int numframes;
+
+	Mesh(const char *mesh_file, const char *tex_file);
+
+	void draw();
+
+	void initShader();
+
+protected:
+	char *vertexShaderCode;
+	char *fragmentShaderCode;
+
+	//std::list<RenderObject> objectList;
+
+private:
+	int mTextureID;
+    int mProgram;
+    int mTexCoordHandle;
+    int mPositionHandle;
+    int mNextPositionHandle;
+    int mAnimProgressHandle;
+    int *mVertexBuffer;
+    int mTexCoordBuffer;
+    int mMVPMatrixHandle;
+
+    //final int vertexStride = 3 * 4; // 4 bytes per vertex
+    //final int texCoordsStride = 2 * 4; // 4 bytes per vertex
+
+	int loadShader(int type, const char *code);
+};
+
+
+
+#endif
